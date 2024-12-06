ed1228e872e39fce1662ea344dd0fa164cf88a32
Amol <amolmohite166@gmail.com>
2024-12-06 14:04:21 +0530

    min change

--- app/controllers/concerns/json_web_token.rb
+++ app/controllers/concerns/json_web_token.rb
@@ -1,7 +1,7 @@
 require 'jwt'
 module JsonWebToken
   extend ActiveSupport::Concern
-  SECRET_KEY = Rails.application.secret_key_base
+  SECRET_KEY = 'aa1728f6b9946b09fdd2e5d934358a9fdeddf996b6c3409c12b5a4b81cb5024124916d322a759abe67013577eedcf2a582a01018955ce4b5a448c5d99d0e40ea'
 
   def jwt_encode(payload, exp=1.days.from_now)
     payload[:exp] = exp.to_i
