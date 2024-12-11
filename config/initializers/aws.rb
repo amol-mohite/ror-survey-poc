e6f192beea28315a43a7540da1e5e6028cfbbbaf
Amol <50193994+amol-mohite@users.noreply.github.com>
2024-12-11 15:55:08 +0530

    Update aws.rb

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,4 +1,4 @@
 Aws.config.update({
   region: 'eu-north-1',
-  credentials: Aws::Credentials.new('AKIAU72LGH64F5XUTUMH', 'wGNHTXStoSX/tZNtjUztxGIS4Z9d5pQygzUU2aXG')
+  credentials: Aws::Credentials.new('YOUR_AWS_CREDENTIALS', 'YOUR_AWS_CREDENTIALS')
 })
